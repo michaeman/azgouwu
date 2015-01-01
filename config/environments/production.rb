fea21266ee420d3f8b1d0f6e5213e16bd8a4b4f0
Peter He <hexinpeter@gmail.com>
2015-01-01 21:40:59 +1100

    aws cdn set up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,10 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  # for aws-cdn, cache properties
+  config.static_cache_control = "public, max-age=1536000"
+  config.action_controller.asset_host = 'd36zhanskdad1p.cloudfront.net'
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
