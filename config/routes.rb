a49fbdf01f48b63f40e3f6f27429ea7f8d404952
Peter He <hexinpeter@gmail.com>
2015-01-01 21:31:11 +1100

    incomplete China tour -v1

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get 'china_tours/signup'
 
-  get 'home/index'
+  get 'home/index', as: 'home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
