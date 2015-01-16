ae575e9ad070d7755ed2b246fcf9cad3b3033a15
Peter He <hexinpeter@gmail.com>
2015-01-16 17:11:58 +1100

    button-link and coming soon tags added ffor the homepage

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -4,12 +4,23 @@ function setImageHeight() {
   $('.manufacture img').css('height', 0.8 * containerWidth + 'px');
 }
 
+// set the size of the button displayed on the home page
+function setButtonSize() {
+  // alert('ha');
+  if ($(window).width() < 768) {
+    $('.button-link').removeClass('btn-lg btn-sm btn-xs');
+  } else if ($(window).width() > 992) {
+    $('.button-link').removeClass('btn-lg btn-sm btn-xs').addClass('btn-lg');
+  }
+}
+
 $(document).ready(function() {
 
   // the responsive images displayed at home page, store furniture (carosel)
   setImageHeight();
   $(window).resize( function() {
     setImageHeight();
+    // setButtonSize();
   });  
 
   // show and hide info, china-tour -> how we can help
@@ -17,10 +28,10 @@ $(document).ready(function() {
     evt.preventDefault();
     $(this).siblings('div').slideToggle(300);
     if ($(this).find('a').text() == "More Info") {
-      $(this).find('a').text("Less")
+      $(this).find('a').text("Less");
     } else {
-      $(this).find('a').text("More Info")
-    };
+      $(this).find('a').text("More Info");
+    }
   });
 
 });
