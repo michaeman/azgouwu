18a345f6fa7d82a544f2964e3e255af1fd55fe23
Peter He <hexinpeter@gmail.com>
2015-01-27 20:24:23 +1100

    reference number uniqueness validated, and generated

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,13 +6,25 @@ class Customer < ActiveRecord::Base
   validates :post_code, format: { with: %r{\d{4}}i }
   validates :addr_state, inclusion: { in: ['VIC', 'NSW', 'ACT', 'NT', 'Qld', 'SA', 'Tas', 'WA'] }
   validates :phone, format: { with: %r{\+?\d{5,15}}i } # a rough guide
+  validates :reference_num, uniqueness: true
 
   # create new Customer accepting arguments from the form input, assign a random reference_num to it
   def Customer.create_without_ref_num(param)
     c = Customer.new(param)
     if c.reference_num == nil
-      c.reference_num = Array.new(10){[*"A".."Z", *"0".."9"].sample}.join
+      c.reference_num = generate_reference_num
     end
     return c
   end
+
+  private 
+    # generate a unique reference number
+    def Customer.generate_reference_num
+      r_num = Array.new(10){[*"A".."Z", *"0".."9"].sample}.join
+      while Customer.find_by reference_num: r_num do 
+        r_num = Array.new(10){[*"A".."Z", *"0".."9"].sample}.join
+      end
+      return r_num 
+    end
+
 end
